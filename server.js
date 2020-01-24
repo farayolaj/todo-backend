e0563c7ac1c132919a8e182ab8921ae29c0d7572
csjoe <tegajoseph2002@gmail.com>
2020-01-24 02:50:21 +0100

    Doing something :)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/auth.route');
 const userRoute = require('./routes/user.route');
+// const createError = require('http-errors');
 
 
 const app = express();
@@ -12,8 +13,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', authRoute);
 app.use('/user', userRoute);
 
-app.listen(1234, error => {
+const PORT = 1234;
+
+app.listen(PORT, error => {
   if (error) console.log(error);
 
-  console.log(`Server now running on port 1234`);
-})
+  console.log(`Server now running on port ${PORT}`);
+});
