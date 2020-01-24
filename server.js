102e045c066054cd01bcc644c4f65bda530b10d0
csjoe <tegajoseph2002@gmail.com>
2020-01-24 03:04:19 +0100

    ...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/auth.route');
 const userRoute = require('./routes/user.route');
@@ -12,6 +13,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', authRoute);
 app.use('/user', userRoute);
+
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect('mongodb://localhost:27017/todo', options)
+  .catch(error => console.error(error));
+
+mongoose.connection.on('connect', () => console.log('Connected to database'));
+
+mongoose.connection.on('error', error => console.error(error));
+
 
 const PORT = 1234;
 
